e1ab18aebab6fd4204c41bd992c6fbc22c0bd704
Ian Mendoza <ian.mendoza9@gmail.com>
2024-12-17 11:50:56 -0700

    returns get requests

--- flask_app.py
+++ flask_app.py
@@ -117,15 +117,11 @@ def webhook_verification():
 
 @app.route('/hilos_webhook', methods=['POST', 'GET'])
 def hilos_webhook_endpoint():
-    # Extract the incoming data
     if request.method == 'GET':
-        # Extract the raw data from the GET request body
-        raw_data = request.get_data(as_text=True)
-        data = json.loads(raw_data) if raw_data else {}
-    else:
-        # For POST requests, continue to use the request.json
-        data = request.json
-        
+        return jsonify({"message": "GET request received, but only POST requests are processed"}), 200
+
+    # Extract the incoming data
+    data = request.json
     if data['event_data']['direction'] != "INBOUND":
         print("not inbound. skipping...")
         
